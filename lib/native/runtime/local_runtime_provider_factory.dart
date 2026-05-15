75d7a83dc9f7a55a1ea58e199f4c57df4ae908fb
pilialvu75-hue <pilialvu75@gmail.com>
2026-05-15 07:50:07 +0200

    Update local_runtime_provider_factory.dart

--- lib/native/runtime/local_runtime_provider_factory.dart
+++ lib/native/runtime/local_runtime_provider_factory.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
-import 'package:ai_orchestrator/core/runtime/inference/android_ffi_runtime_provider.dart';
+import 'package:ai_orchestrator_riserva/lib/core/runtime/inference/android_ffi_runtime_provider.dart';
 import 'package:ai_orchestrator_riserva/lib/core/runtime/inference/local_runtime_provider.dart';
 
 /// Returns the [LocalRuntimeProvider] appropriate for the current platform.
