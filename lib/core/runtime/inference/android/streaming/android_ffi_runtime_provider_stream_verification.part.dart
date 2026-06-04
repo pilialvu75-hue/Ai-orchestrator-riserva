79771ec35c27373182a3393088955e35144b04b7
pilialvu75-hue <pilialvu75@gmail.com>
2026-06-04 20:45:19 +0200

    Update part declaration in android_ffi_runtime_provider_stream_verification

--- lib/core/runtime/inference/android/streaming/android_ffi_runtime_provider_stream_verification.part.dart
+++ lib/core/runtime/inference/android/streaming/android_ffi_runtime_provider_stream_verification.part.dart
@@ -1,4 +1,5 @@
-part of runtime_core;
+part of '../../runtime_core.dart';
+
 
 extension AndroidFfiRuntimeStreamingVerificationExtension on AndroidFfiRuntimeProvider {
   TokenStream streamVerificationInference({
