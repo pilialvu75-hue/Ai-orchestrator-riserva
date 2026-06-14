903728b8cbc3eab410563cef8243aebb8f921c60
pilialvu75-hue <pilialvu75@gmail.com>
2026-06-14 16:46:48 +0200

    Add WorkspaceDiffBuilder to compute file diffs
    Implement a class to calculate structural deltas between two states of the virtual file system.

---
+++ lib/features/ai_code/workspace/services/workspace_diff_builder.dart
@@ -0,0 +1,25 @@
+import '../models/file_diff.dart';
+
+/// Calcola i delta strutturali staccando la generazione del diff logico 
+/// dallo stato mutabile del workspace.
+class WorkspaceDiffBuilder {
+  /// Confronta due istantanee del VFS ed emette la lista dei delta calcolati.
+  List<FileDiff> buildDiffs(Map<String, String> oldState, Map<String, String> newState) {
+    final List<FileDiff> diffs = [];
+    final allKeys = {...oldState.keys, ...newState.keys};
+
+    for (final key in allKeys) {
+      final oldContent = oldState[key];
+      final newContent = newState[key];
+
+      if (oldContent != newContent) {
+        diffs.add(FileDiff(
+          filePath: key,
+          originalContent: oldContent,
+          updatedContent: newContent,
+        ));
+      }
+    }
+    return diffs;
+  }
+}
