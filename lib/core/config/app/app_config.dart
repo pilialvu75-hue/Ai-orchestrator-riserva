2c1e6889309ba92830b919d9b10a0f35c1beddb6
pilialvu75-hue <pilialvu75@gmail.com>
2026-08-21 17:46:31 +0200

    Delete AppConstants class and associated constants
    Removed AppConstants class and its constants related to STT and TTS.

--- lib/core/config/app/app_config.dart
+++ lib/core/config/app/app_config.dart
@@ -9,29 +9,3 @@ class AppConfig {
     environment: EnvironmentConfig.current,
   );
 }
-
-class AppConstants {
-  const AppConstants._();
-
-  // App Info
-  static const String appName = 'AI Orchestrator';
-
-  // Voice Engine - STT (Sherpa-ONNX Zipformer)
-  static const String sttEncoderFile = 'encoder.onnx';
-  static const String sttDecoderFile = 'decoder.onnx';
-  static const String sttJoinerFile = 'joiner.onnx';
-  static const String sttTokensFile = 'tokens.txt';
-
-  static const String sttZipformerTarUrl =
-      'https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-streaming-zipformer-en-2023-06-26.tar.bz2';
-  static const int sttZipformerTarExpectedBytes = 223522304;
-
-  // Voice Engine - TTS (VITS Piper Paola)
-  static const String ttsModelFile = 'model.onnx';
-  static const String ttsTokensFile = 'tts_tokens.txt';
-  static const String ttsEspeakDataDir = 'espeak-ng-data';
-
-  static const String ttsPaolaTarUrl =
-      'https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-it_IT-paola-medium.tar.bz2';
-  static const int ttsPaolaTarExpectedBytes = 65000000;
-}
