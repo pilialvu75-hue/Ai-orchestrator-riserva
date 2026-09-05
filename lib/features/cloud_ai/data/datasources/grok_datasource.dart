721e24ddba1a29002ce4f3d703fbb25be2e49000
pilialvu75-hue <pilialvu75@gmail.com>
2026-09-05 11:31:12 +0200

    cloud: connect Grok adapter to secure store

--- lib/features/cloud_ai/data/datasources/grok_datasource.dart
+++ lib/features/cloud_ai/data/datasources/grok_datasource.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:ai_orchestrator/core/config/app/app_constants.dart';
 import 'package:ai_orchestrator/core/error/exceptions.dart';
+import 'package:ai_orchestrator/core/runtime/inference/cloud_credential_store.dart';
 import 'package:ai_orchestrator/features/cloud_ai/data/models/ai_request_model.dart';
 import 'package:ai_orchestrator/features/cloud_ai/data/models/ai_response_model.dart';
 import 'package:http/http.dart' as http;
@@ -12,7 +13,8 @@ class GrokDataSource {
     String Function()? apiKeyProvider,
     http.Client? httpClient,
     this.model = 'grok-4.6',
-  })  : _apiKeyProvider = apiKeyProvider ?? (() => apiKey),
+  })  : _apiKeyProvider = apiKeyProvider ??
+            (() => CloudCredentialStore.instance.secretFor('grok') ?? apiKey),
         _client = httpClient ?? http.Client();
 
   final String Function() _apiKeyProvider;
