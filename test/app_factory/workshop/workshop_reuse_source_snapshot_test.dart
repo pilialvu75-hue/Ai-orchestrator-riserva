9bae3f1ef100fa0a879b404401fd1f2c87415ac9
pilialvu75-hue <pilialvu75@gmail.com>
2026-09-13 14:03:06 +0200

    test(workshop): use configured gateway root for snapshot staging

--- test/app_factory/workshop/workshop_reuse_source_snapshot_test.dart
+++ test/app_factory/workshop/workshop_reuse_source_snapshot_test.dart
@@ -79,7 +79,6 @@ void main() {
           id: 'new-project',
           title: 'New project',
           instruction: 'Reuse invoice foundation',
-          projectPath: '.',
         ),
         gateway: LocalGitWorkspaceGateway(rootPath: target.path),
       );
