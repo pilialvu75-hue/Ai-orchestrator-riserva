e80c6a21181f0692871a3b09a7d24746b1fc34ff
pilialvu75-hue <pilialvu75@gmail.com>
2026-09-18 21:12:34 +0200

    fix(memory): store single Unicode property escapes

--- lib/features/semantic_index/workspace_embedding_service.dart
+++ lib/features/semantic_index/workspace_embedding_service.dart
@@ -28,7 +28,7 @@ class WorkspaceEmbeddingService {
       return vector;
     }
     final tokens = normalized
-        .split(RegExp(r'[^\\p{L}\\p{N}_]+', unicode: true))
+        .split(RegExp(r'[^\p{L}\p{N}_]+', unicode: true))
         .where((token) => token.isNotEmpty)
         .toList(growable: false);
     if (tokens.isEmpty) return vector;
